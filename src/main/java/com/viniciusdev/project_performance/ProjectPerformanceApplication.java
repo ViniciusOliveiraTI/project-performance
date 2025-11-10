4ff54a186dcf59b928887aeaa60cbdebb7752c5a
ViniciusOliveiraTechnik <vinicius.oliveira@grupotechnik.com.br>
2025-11-10 15:23:30 -0300

    Project first config

---
+++ src/main/java/com/viniciusdev/project_performance/ProjectPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusdev.project_performance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPerformanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusdev/project_performance/ProjectPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusdev.project_performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
